2d5f354612858773caffac15c17e57626d8325de
Chuy <chuymaster3@hotmail.com>
2018-09-16 15:19:36 +0900

    structs lesson

--- cards/structs/main.go
+++ cards/structs/main.go
@@ -4,9 +4,15 @@ import (
 	"fmt"
 )
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
@@ -24,4 +30,49 @@ func main() {
 	john.firstName = "John"
 	john.lastName = "Doe"
 	fmt.Println(john)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		}, // Multiline declaration requires comma at the end
+	}
+
+	jim.print()
+	jim.updateName("Jimmy") // This will not work because GO is pass by value language
+	jim.print()
+
+	// *** Everything in GO is passed by value. Everything is copied before passing. **
+
+	jimPointer := &jim // & creates a pointer to jim
+	jimPointer.updateNameUsingPointer("Jimmy")
+	jim.print()
+
+	jane := person{
+		firstName: "Jane",
+		lastName:  "Pork",
+		contactInfo: contactInfo{
+			email:   "jane@gmail.com",
+			zipCode: 94000,
+		}, // Multiline declaration requires comma at the end
+	}
+	jane.print()
+	jane.updateNameUsingPointer("Janny") // Shortcut. No need to assign this as pointer because GO converts it automatically.
+	jane.print()
+}
+
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+// Start operator change the variable to pointer type
+func (p *person) updateNameUsingPointer(newFirstName string) {
+	(*p).firstName = newFirstName // (*) turns the pointer to the value inside the memory
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+	fmt.Println("")
 }
