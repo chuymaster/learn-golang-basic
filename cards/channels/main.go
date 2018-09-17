8fcbaae1593d673176b699aaad77943bb4aaddc0
Chuy <chuymaster3@hotmail.com>
2018-09-17 10:46:52 +0900

    Use function literal for sleep

--- cards/channels/main.go
+++ cards/channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -23,10 +24,26 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c) // (<-c) waits for value to be sent. This is blocking the thread.
-	}
+	// for i := 0; i < len(links); i++ {
+	// 	fmt.Println(<-c) // (<-c) waits for value to be sent. This is blocking the thread.
+	// }
 
+	// // Infinite loop after finish checking one link
+	// for {
+	// 	go checkLink(<-c, c)
+	// }
+
+	// Wait for channel to return value, assign to l and run functions
+	// Same as infinite loop. But more understandable
+	for l := range c {
+		// time.Sleep(5 * time.Second) // This will block main routine. NG
+
+		go func(link string) { // Function Literal. Used to pass function as argument
+			time.Sleep(5 * time.Second)
+			checkLink(link, c) // Pass l as value to the Function Literal to copy it (by value).
+		}(l)
+		// go checkLink(l, c)
+	}
 }
 
 // channel of string
@@ -34,10 +51,10 @@ func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
-		c <- "Might be down I think"
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
-	c <- "Yep it's up" // c <- "xxx" sends message to channel
+	c <- link // c <- "xxx" sends message to channel
 }
